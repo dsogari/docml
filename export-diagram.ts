a75f8cbf0cff8b5101aca0074b8797ceaf1bcb26
Diego Sogari <diego.sogari@gmail.com>
2025-03-11 05:19:39 -0300

    Add diagram export script

---
+++ export-diagram.ts
@@ -0,0 +1,24 @@
+#!/usr/bin/env bun
+import { EmptyFileSystem, createParser, LangiumParser } from 'langium';
+import { createSyntaxDiagramsCode } from 'chevrotain';
+import { createDocmlServices } from './src/language/docml-module.js';
+import { writeFileSync } from 'fs';
+
+class CustomLangiumParser extends LangiumParser {
+  exportHtmlDiagram(): string {
+    const rules = this.wrapper.getSerializedGastProductions();
+    return createSyntaxDiagramsCode(rules);
+  }
+}
+
+const { Docml } = createDocmlServices(EmptyFileSystem);
+const { definition } = Docml.parser.Lexer;
+const parser = new CustomLangiumParser(Docml);
+createParser(Docml.Grammar, parser, definition).finalize();
+const html = parser.exportHtmlDiagram();
+
+if (process.argv.length > 2) {
+  writeFileSync(process.argv[2], html);
+} else {
+  console.log(html);
+}
